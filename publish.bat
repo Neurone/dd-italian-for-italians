e8eeec10f1019e2fc4c56d6b54118aeb2e1519ef
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2019-04-25 02:00:23 +0200

    Diverse traduzioni (Gruppo diventa Squadra, ecc.) Aggiornato script di publishing;

--- publish.bat
+++ publish.bat
@@ -1,3 +1,5 @@
-cd D:/Giochi/Steam/SteamApps/common/DarkestDungeon/_windows
+del mod\modfiles.txt
+del mod\steam_workshop_upload.log
+cd D:\Giochi\Steam\SteamApps\common\DarkestDungeon\_windows
 steam_workshop_upload.exe "d:\Sviluppo\dd-italian-for-italians\mod\project.xml"
-cd D:/Sviluppo/dd-italian-for-italians
+cd D:\Sviluppo\dd-italian-for-italians
